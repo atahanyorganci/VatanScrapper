78845e57d8cd3eda3c2efdd352b1e00d46956dfd
Atahan Yorganci <atahanyorganci@hotmail.com>
2019-06-17 22:51:14 +0300

    Format with autopep8 and replace " with '

--- log.py
+++ log.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
-import traceback, sys
+import traceback
+import sys
 
 INFO = 0
 DEBUG = 1
@@ -9,7 +10,7 @@ CRITICAL = 4
 
 
 class Logger:
-    def __init__(self, level: int, timed=True, time_str="%H:%M:%S"):
+    def __init__(self, level: int, timed=True, time_str='%H:%M:%S'):
         self.__level = level
         self.__timed = timed
         self.__time_str = time_str
@@ -25,49 +26,49 @@ class Logger:
         func()
         end = timedelta()
         delta = timedelta.total_seconds(end - start)
-        self.info(f"{func} took {delta} seconds.")
+        self.info(f'{func} took {delta} seconds.')
 
     def verbose(self, message):
-        dt = datetime.now().strftime("%d %B %Y %H:%M:%S")
-        print(f"VERBOSE: {dt}: {message}")
+        dt = datetime.now().strftime('%d %B %Y %H:%M:%S')
+        print(f'VERBOSE: {dt}: {message}')
 
     def info(self, message: str):
         if INFO >= self.__level:
             if self.__timed:
-                print(f"INFO: {self._getTime()}: {message}")
+                print(f'INFO: {self._getTime()}: {message}')
             else:
-                print(f"INFO: {message}")
+                print(f'INFO: {message}')
 
     def debug(self, message: str):
         if DEBUG >= self.__level:
             if self.__timed:
-                print(f"DEBUG: {self._getTime()}: {message}")
+                print(f'DEBUG: {self._getTime()}: {message}')
             else:
-                print(f"DEBUG: {message}")
+                print(f'DEBUG: {message}')
 
     def warning(self, message: str):
         if WARNING >= self.__level:
             if self.__timed:
-                print(f"WARNING: {self._getTime()}: {message}")
+                print(f'WARNING: {self._getTime()}: {message}')
             else:
-                print(f"WARNING: {message}")
+                print(f'WARNING: {message}')
 
     def error(self, message: str):
         if ERROR >= self.__level:
             if self.__timed:
-                print(f"ERROR: {self._getTime()}: {message}")
+                print(f'ERROR: {self._getTime()}: {message}')
             else:
-                print(f"ERROR: {message}")
+                print(f'ERROR: {message}')
 
     def critical(self, message: str):
         if INFO >= self.__level:
             if self.__timed:
-                print(f"CRITICAL: {self._getTime()}: {message}")
+                print(f'CRITICAL: {self._getTime()}: {message}')
             else:
-                print(f"CRITICAL: {message}")
+                print(f'CRITICAL: {message}')
 
     def _getTime(self):
         return datetime.now().strftime(self.__time_str)
-    
+
     def trace(self, ex):
         traceback.print_exception(type(ex), ex, None)
--- notebooks.py
+++ notebooks.py
@@ -12,18 +12,18 @@ def escapeTUR(string):
     Escapes Turkish literals
     '''
     out = string[:]
-    if "ö" in string:
-        out = out.replace('ğ', "o")
-    if "ü" in string:
-        out = out.replace('ü', "u")
-    if "ş" in string:
-        out = out.replace('ş', "s")
-    if 'ı' in string:
-        out = out.replace('ı', "i")
     if 'ö' in string:
-        out = out.replace('ö', "o")
+        out = out.replace('ğ', 'o')
+    if 'ü' in string:
+        out = out.replace('ü', 'u')
+    if 'ş' in string:
+        out = out.replace('ş', 's')
+    if 'ı' in string:
+        out = out.replace('ı', 'i')
+    if 'ö' in string:
+        out = out.replace('ö', 'o')
     if 'ç' in string:
-        out = out.replace('ç', "c")
+        out = out.replace('ç', 'c')
     return out
 
 
@@ -37,33 +37,33 @@ def getSpecs(url: str, logger=None, verbose=False):
     # Process given URL
     try:
         with requests.get(url) as response:
-            soup = BeautifulSoup(response.text, "html.parser")
+            soup = BeautifulSoup(response.text, 'html.parser')
             price = soup.find(
-                "span", {"class": "urunDetay_satisFiyat"}).text.split(" ")[0]
-            spec_cells = soup.find("div", {"class": "urunOzellik"}).findAll(
-                "td", {"class": "gridAlternateDefault gridAlternateUrunOzellik"})
+                'span', {'class': 'urunDetay_satisFiyat'}).text.split(' ')[0]
+            spec_cells = soup.find('div', {'class': 'urunOzellik'}).findAll(
+                'td', {'class': 'gridAlternateDefault gridAlternateUrunOzellik'})
             manufacturer = soup.find(
-                "h1", {"class": "emos_H1"}).text.split(" ")[0]
-            name = soup.find("div", {"id": "plhUrunKodu"}).text.strip()
+                'h1', {'class': 'emos_H1'}).text.split(' ')[0]
+            name = soup.find('div', {'id': 'plhUrunKodu'}).text.strip()
             content = []
             for cell in spec_cells:
                 properties = {}
-                properties["name"] = cell.find("div").text.strip()
-                for pair in cell.findAll("tr"):
-                    k = escapeTUR(pair.findAll("td")[
+                properties['name'] = cell.find('div').text.strip()
+                for pair in cell.findAll('tr'):
+                    k = escapeTUR(pair.findAll('td')[
                                   0].text[:-6].strip().lower())
-                    v = escapeTUR(pair.findAll("td")[
+                    v = escapeTUR(pair.findAll('td')[
                                   1].text[:-6].strip().lower())
                     properties[k] = v
                 content.append(properties)
     except ConnectionError:
-        logger.critical("Failed to locate URL, ConnectionError has occurred.")
-        logger.critical("Quitting...")
+        logger.critical('Failed to locate URL, ConnectionError has occurred.')
+        logger.critical('Quitting...')
         return
     except Exception as ex:
-        logger.critical("Unexpected type of error has occurred.")
+        logger.critical('Unexpected type of error has occurred.')
         logger.trace(ex)
-        logger.critical("Quitting...")
+        logger.critical('Quitting...')
         return
 
     # If verbose mode is turned return all the properties
@@ -72,28 +72,28 @@ def getSpecs(url: str, logger=None, verbose=False):
 
     # Return desired properties as a tuple
     try:
-        CPU = content[0]["i̇slemci teknolojisi"] + \
-            "-" + content[0]["i̇slemci numarasi"]
-        size = content[1]["ekran boyu"]
-        resolution = content[1]["cozunurluk (piksel)"]
-        storage = str(content[2]["disk kapasitesi"]) + \
-            " " + str(content[2]["disk turu"])
-        os = content[0]["i̇sletim sistemi"]
+        CPU = content[0]['i̇slemci teknolojisi'] + \
+            '-' + content[0]['i̇slemci numarasi']
+        size = content[1]['ekran boyu']
+        resolution = content[1]['cozunurluk (piksel)']
+        storage = str(content[2]['disk kapasitesi']) + \
+            ' ' + str(content[2]['disk turu'])
+        os = content[0]['i̇sletim sistemi']
         return (manufacturer, name, price, CPU, size, resolution, storage, os)
     except KeyError:
-        logger.error("Failed to locate properties, returning None.")
+        logger.error('Failed to locate properties, returning None.')
         return None
     except Exception as ex:
-        logger.critical("Unexpected type of error has occurred.")
+        logger.critical('Unexpected type of error has occurred.')
         logger.trace(ex)
-        logger.critical("Quitting...")
+        logger.critical('Quitting...')
         sys.exit(1)
 
 
 def main():
     # Constants
-    BASE_URL = "http://www.vatanbilgisayar.com"
-    OUT_FILE = "outfile.xlsx"
+    BASE_URL = 'http://www.vatanbilgisayar.com'
+    OUT_FILE = 'outfile.xlsx'
 
     # Instantiate Logger Object and configure it
     logger = log.Logger(log.INFO)
@@ -102,65 +102,65 @@ def main():
     try:
         workbook = openpyxl.load_workbook('template.xlsx')
         out_sheet = workbook[workbook.sheetnames[0]]
-        logger.info("Template .xlsx file loaded successfully.")
+        logger.info('Template .xlsx file loaded successfully.')
     except FileNotFoundError as ex:
-        logger.critical("Template .xlsx file not found.")
-        logger.critical("Quitting...")
+        logger.critical('Template .xlsx file not found.')
+        logger.critical('Quitting...')
         return
     except Exception as ex:
-        logger.critical("Unexpected type of error has occurred.")
+        logger.critical('Unexpected type of error has occurred.')
         logger.trace(ex)
-        logger.critical("Quitting...")
+        logger.critical('Quitting...')
         sys.exit(1)
 
     # Grab product links
     links = []
     try:
         for i in range(1, 9):
-            web_url = f"{BASE_URL}/notebook/?page={i}"
+            web_url = f'{BASE_URL}/notebook/?page={i}'
             main_page = requests.get(web_url)
-            vatan = BeautifulSoup(main_page.text, "html.parser")
-            logger.debug(f"Product list {i} returned {main_page.status_code}.")
-            notebooks = vatan.findAll("div", {"class": "ems-prd-inner"})
+            vatan = BeautifulSoup(main_page.text, 'html.parser')
+            logger.debug(f'Product list {i} returned {main_page.status_code}.')
+            notebooks = vatan.findAll('div', {'class': 'ems-prd-inner'})
             for notebook in notebooks[:-1]:
-                url = notebook.find('div').a["href"]
+                url = notebook.find('div').a['href']
                 links.append(url)
-            logger.info(f"Processed product list {i}.")
-            logger.info(f"Total number of products is {len(links)}.")
+            logger.info(f'Processed product list {i}.')
+            logger.info(f'Total number of products is {len(links)}.')
     except ConnectionError:
-        logger.critical("Failed to locate URL, ConnectionError has occurred.")
-        logger.critical("Quitting...")
+        logger.critical('Failed to locate URL, ConnectionError has occurred.')
+        logger.critical('Quitting...')
         return
     except Exception as ex:
-        logger.critical("Unexpected type of error has occurred.")
+        logger.critical('Unexpected type of error has occurred.')
         logger.trace(ex)
-        logger.critical("Quitting...")
+        logger.critical('Quitting...')
         sys.exit(1)
-    logger.info(f"Processed all product lists total number of products is {len(links)}.")
+    logger.info(f'Processed all product lists total number of products is {len(links)}.')
 
     # Process product links
     row = 2
-    error = {"count": 0, "links": []}
+    error = {'count': 0, 'links': []}
     for i, link in enumerate(links):
         # Gather data from the products page
         try:
-            spec_url = f"{BASE_URL}{link}#urun-ozellikleri"
+            spec_url = f'{BASE_URL}{link}#urun-ozellikleri'
             result = getSpecs(spec_url, logger=logger)
             if result is None:
-                error["count"] += 1
-                error["links"].append(link)
+                error['count'] += 1
+                error['links'].append(link)
                 continue
             manufacturer, name, price, CPU, size, resolution, storage, os = result
-            logger.info(f"Link number {i} specs processed.")
+            logger.info(f'Link number {i} specs processed.')
         except ConnectionError:
             logger.critical(
-                "Failed to locate URL, ConnectionError has occurred.")
-            logger.critical("Quitting...")
+                'Failed to locate URL, ConnectionError has occurred.')
+            logger.critical('Quitting...')
             return
         except Exception as ex:
-            logger.critical("Unexpected type of error has occurred.")
+            logger.critical('Unexpected type of error has occurred.')
             logger.trace(ex)
-            logger.critical("Quitting...")
+            logger.critical('Quitting...')
             sys.exit(1)
 
         # Write gathered data into the spreadsheet
@@ -174,22 +174,22 @@ def main():
             out_sheet.cell(row=row, column=7, value=storage)
             out_sheet.cell(row=row, column=8, value=os)
             out_sheet.cell(row=row, column=9, value=link)
-            logger.info(f"Link number {i} written to the file.")
+            logger.info(f'Link number {i} written to the file.')
             row += 1
             workbook.save(OUT_FILE)
         except PermissionError:
-            logger.critical(f"Permission denied when accessing file ({OUT_FILE}).")
-            logger.critical("Quitting...")
+            logger.critical(f'Permission denied when accessing file ({OUT_FILE}).')
+            logger.critical('Quitting...')
             return
         except Exception as ex:
-            logger.critical("Unexpected type of error has occurred.")
+            logger.critical('Unexpected type of error has occurred.')
             logger.trace(ex)
-            logger.critical("Quitting...")
+            logger.critical('Quitting...')
             sys.exit(1)
-    logger.debug(f'Skipped {error["count"]} links.')
-    for link in error["links"]:
-        logger.info(f"{BASE_URL}{link}#urun-ozellikleri")
+    logger.debug(f'Skipped {error['count']} links.')
+    for link in error['links']:
+        logger.info(f'{BASE_URL}{link}#urun-ozellikleri')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
